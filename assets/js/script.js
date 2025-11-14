c72413fbd9e7cf99212fbc0302f41fbf45f705fd
Hussein Zayat <husseinzayat@Husseins-MacBook-Pro.local>
2025-11-14 14:17:33 +0100

    repovate: automated fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-```javascript
 function setTheme(t){ 
   root.setAttribute('data-theme', t); 
   localStorage.setItem('theme', t); 
-  if (icon) icon.setAttribute('name', t==='dark'?'sunny-outline':'moon-outline'); 
+  if (typeof icon !== 'undefined' && icon) icon.setAttribute('name', t==='dark'?'sunny-outline':'moon-outline'); 
 }
 setTheme(theme);
 ```
